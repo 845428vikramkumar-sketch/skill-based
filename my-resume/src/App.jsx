211f9bd6de60ab5ee19c6b7bd6fcc33ad8fcf88c
vikram <845428vikramkumar@gmail.com>
2026-08-04 22:32:43 +0530

    my first resume

---
+++ my-resume/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Mera Naam</h1>
+      <p>Main Patti, UP se hun</p>
+      <h2>Skills: HTML, CSS, React</h2>
+    </div>
+  )
+}
+export default App;
